a2a40664314460a10204c1f90dcde6199f86e2c8
roman <romansheikh170@gmail.com>
2021-10-04 01:37:29 +0600

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <BrowserRouter>
+        <Header></Header>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png'
 
 // Header components
 const Header = () => {
     return (
-        <div className="bg-blue-500">
+        <div className="flex justify-between items-center bg-blue-500 py-2 px-4">
             {/* logo section */}
             <div className="flex items-center">
                 <img className="w-logo-img" src={logo} alt="" />
                 <div className="text-white ml-2">
-                    <h2>Instu</h2>
+                    <h2 className="text-4xl text-yellow-500">Instu</h2>
                     <p>Music Classes</p>
                 </div>
+            </div>
+            {/* nav bar section */}
+            <div className="flex text-xl">
+            <NavLink to="/home" className="text-white mx-2" activeClassName="text-yellow-500">Home</NavLink>
+            <NavLink to="/services" className="text-white mx-2" activeClassName="text-yellow-500">Services</NavLink>
+            <NavLink to="/about" className="text-white mx-2" activeClassName="text-yellow-500">About</NavLink>
+            <NavLink to="/contact" className="text-white mx-2" activeClassName="text-yellow-500">Contact</NavLink>
+            <NavLink to="/contact" className="text-white mx-2" activeClassName="text-yellow-500">
+                    <F
+            </NavLink>
             </div>
         </div>
     );
