9ac6860b9c1096854d2809f47823f85d21ac4a91
roman <romansheikh170@gmail.com>
2021-10-04 10:14:02 +0600

    --- Auto-Git Commit ---

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import map from '../../images/map.jpeg'
 
 const Contact = () => {
     return (
-        <div>
+        <div >
             <h1 className="text-4xl font-bold text-center my-8">Contact With Us</h1>
-            <div></div>
+            <div className="grid grid-cols-2 gap-8 px-8 my-8">
+                <div>
+                    <img src={map} alt="" />
+                </div>
+                <div>
+                    <input type="email" placeholder="Enter Your Email" /><br />
+                    <textarea placeholder="type your message" cols="30" rows="10"></textarea><br />
+                    <button>Submit Your Mail</button>
+                </div>
+            </div>
         </div>
     );
 };
